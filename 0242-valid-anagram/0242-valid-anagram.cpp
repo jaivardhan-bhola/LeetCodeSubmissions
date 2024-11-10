4f79de53574ac2041a38a9c953c14a1182ceb4a1
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2024-11-10 10:18:53 +0530

    Time: 3 ms (66.3%), Space: 8.6 MB (93.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         sort(s.begin(), s.end());
-        sort(t.begin(),t.end());
-        if (s==t) return true; return false;
+        sort(t.begin(), t.end());
+        if (s == t)
+            return true;
+        return false;
     }
 };
