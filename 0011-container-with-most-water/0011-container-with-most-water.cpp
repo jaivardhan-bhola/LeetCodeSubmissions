1358dbc83fa7270ab85aa2896b25e45685ab581a
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2024-11-12 10:03:23 +0530

    Time: 0 ms (100%), Space: 62.3 MB (33.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,10 +3,9 @@ public:
     int maxArea(vector<int>& height) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        int a=0, b=height.size()-1, area,ans=0;
+        int a=0, b=height.size()-1,ans=0;
         while(a<b){
-            area=min(height[a],height[b])*(b-a);
-            ans=max(ans,area);
+            ans=max(ans,min(height[a],height[b])*(b-a));
             if (height[a]<height[b]) a++;
             else b--;
         }
