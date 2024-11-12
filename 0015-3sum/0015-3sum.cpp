6ad1fbc6635d60bcf244cf30976d75533095815d
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2024-11-12 09:07:32 +0530

    Time: 38 ms (97.44%), Space: 27.4 MB (89.2%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int a, b, sum;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            a = i + 1;
+            b = nums.size() - 1;
+            while (a < b) {
+                sum = nums[i] + nums[a] + nums[b];
+                if (sum > 0)
+                    b--;
+                else if (sum < 0)
+                    a++;
+                else {
+                    ans.push_back({nums[i], nums[a], nums[b]});
+                    a++;
+                    while (nums[a] == nums[a - 1] && a < b)
+                        a++;
+                }
+            }
+        }
+        return ans;
+    }
+};
