b7cbef1de80339fe3fa9a8440abfe208af81288b
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2024-11-16 14:34:46 +0530

    Time: 59 ms (38.74%), Space: 83 MB (53.68%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.cpp
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -3,14 +3,19 @@ public:
     int carFleet(int target, vector<int>& pos, vector<int>& speed) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        map<int, double> m;
-        for (int i = 0; i < pos.size(); i++)
-            m[-pos[i]] = (double)(target - pos[i]) / speed[i];
-        int ans = 0;
-        double a = 0;
-        for (auto it : m)
-            if (it.second > a)
-                a = it.second, ans++;
-        return ans;
+        vector<pair<int, double>> cars(pos.size());
+        for (int i = 0; i < pos.size(); i++) {
+            cars[i] = {pos[i], (double)(target - pos[i]) / speed[i]};
+        }
+        sort(cars.rbegin(), cars.rend());
+        stack<double> times;
+        for (auto& car : cars) {
+            double time = car.second;
+            if (times.empty() || time > times.top()) {
+                times.push(time);
+            }
+        }
+
+        return times.size();
     }
 };
