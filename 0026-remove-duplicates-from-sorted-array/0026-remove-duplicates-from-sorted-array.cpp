8be1a0a63b1be1ead5b9acfade66309b8d16453b
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-07-26 13:41:09 +0530

    Time: 0 ms (100%), Space: 22.6 MB (51.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,13 +3,15 @@ public:
     int removeDuplicates(vector<int>& nums) {
         cin.tie(nullptr);
         ios::sync_with_stdio(false);
-        set<int> a;
-        for(int i:nums) a.insert(i);
-        int index=0;
-       for(int i:a){
-        nums[index] = i;
-        index++;
-       }
-        return a.size();
+        if (nums.empty()) return 0;
+        int i = 1;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;        
     }
 };
