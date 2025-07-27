34f937ebb1e46fe39bc0ae870b8af1346983ea65
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-07-27 14:20:33 +0530

    Time: 0 ms (100%), Space: 22 MB (10.9%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         cin.tie(nullptr);
         ios::sync_with_stdio(false);
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if (nums[i]!=i) return i;
+        int sum = nums.size()*(nums.size()+1)/2;
+        for(int i:nums){
+            sum-=i;
         }
-        return nums.size();
+        return sum;
     }
 };
