a515453dad62bc036e5ff783938a7163450bc8b0
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-09-25 00:37:26 +0530

    56 ms (44.36%) 50.9 MB (21.6%) 0560-subarray-sum-equals-k 09-25-2025

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int, int> hash;
+        unordered_map<int, int> hash;
         hash[0]=1;
         int pre=0, c=0, a;
         for(int x: nums){
