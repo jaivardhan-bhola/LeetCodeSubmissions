a787c7b7a75e662f86b567e6d325fd0afa683a25
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-09-26 19:24:10 +0530

    0 ms (100%) 31.4 MB (9.47%) 0704-binary-search 09-26-2025

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bs(vector<int>& nums, int low, int high, int target) {
+        if (low > high) return -1;
+        int mid = (high + low) / 2; 
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < target)
+            return bs(nums, mid + 1, high, target);
+        else
+            return bs(nums, low, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return bs(nums, 0, nums.size() - 1, target);
+    }
+};
