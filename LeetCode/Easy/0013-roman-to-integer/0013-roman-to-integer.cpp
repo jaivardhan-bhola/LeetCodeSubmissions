12a66c08cb327850b4d5d49a88496bf4fca7aaac
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-09-28 16:34:28 +0530

    9 ms (34.69%) 13.5 MB (18.57%) 0013-roman-to-integer 09-28-2025

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.cpp
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    int sum = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'I') {
-            if (s[i+1] == 'V') {
-                sum += 4;
-                i++;
-            } else if (s[i+1] == 'X') {
-                sum += 9;
-                i++;
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int sum = 0;
+        int n = s.size();
+        
+        for (int i = 0; i < n; ++i) {
+            int currentVal = roman[s[i]];
+            int nextVal = (i + 1 < n) ? roman[s[i + 1]] : 0;
+            
+            if (currentVal < nextVal) {
+                sum -= currentVal;
             } else {
-                sum += 1;
+                sum += currentVal;
             }
-        } else if (s[i] == 'V') {
-            sum += 5;
-        } else if (s[i] == 'X') {
-            if (s[i+1] == 'L') {
-                sum += 40;
-                i++;
-            } else if (s[i+1] == 'C') {
-                sum += 90;
-                i++;
-            } else {
-                sum += 10;
-            }
-        } else if (s[i] == 'L') {
-            sum += 50;
-        } else if (s[i] == 'C') {
-            if (s[i+1] == 'D') {
-                sum += 400;
-                i++;
-            } else if (s[i+1] == 'M') {
-                sum += 900;
-                i++;
-            } else {
-                sum += 100;
-            }
-        } else if (s[i] == 'D') {
-            sum += 500;
-        } else if (s[i] == 'M') {
-            sum += 1000;
         }
+        
+        return sum;
     }
-    return sum;
-}
 };
