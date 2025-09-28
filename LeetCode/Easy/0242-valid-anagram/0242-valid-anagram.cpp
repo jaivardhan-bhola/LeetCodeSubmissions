ab2382c6b497360a9c238a720c7339273e327bf1
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-09-28 15:58:59 +0530

    5 ms (29.61%) 9.6 MB (74.34%) 0242-valid-anagram 09-28-2025

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<int> a (26,0);
-        for(char c: s) a[c-97]++;
-        for (char c:t) a[c-97]--;
-        for (int b:a) if (b!=0) return false; return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
     }
 };
