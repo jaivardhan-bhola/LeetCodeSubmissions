e93cdb29d32c1957ffd6f6c80361707ae84f32c2
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-10-09 18:03:19 +0530

    N/A (0%) N/A (0%) 0135-candy 10-09-2025

---
+++ LeetCode/Hard/0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int ans=1, peak, down, i=1;
+        while(i<ratings.size()){
+            if (ratings[i]==ratings[i-1]){
+                i++;ans++; continue;
+            }
+            peak=1;
+            while(i<ratings.size() && ratings[i]>ratings[i-1]){
+                peak++; ans+=peak;i++;
+            }
+            down=1;
+            while(i<ratings.size() && ratings[i]<ratings[i-1]){
+                down++; ans+=down;i++;
+            }
+            if (down>peak){
+                ans+=down-peak;
+            }
+
+        }
+        return ans;
+    }
+};
